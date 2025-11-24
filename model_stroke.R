e5f44f851712bb853f8231b76ae481eaf2f61e26
oliviafu <qf35@duke.edu>
2025-11-23 22:14:53 -0500

    revise predictors

--- model_stroke.R
+++ model_stroke.R
@@ -21,30 +21,24 @@ model {
   # 2. Notify must depend on Transport
   # 3. State conditional independence assumptions & justifications
   
-  # Race | Site
+  # Age | Notify, Transport, Race, Site, Z
   for (i in 1:N){
-    Race[i] ~ dcat(pi_race[Site[i],1:3])
+    Age[i] ~ dnorm(mu_A[i], tau_A)
+    mu_A[i] <- eta0
+              + eta_site[Site[i]]
+              + eta_race[Race[i]]
+              + eta_time[Time[i]]
   }
-  for (s in 1:9){
-    pi_race[s,1:3] ~ ddirch(alpha[1:3])
-  }
-
-  alpha[1] <- 0.5  # Jeffreys prior
-  alpha[2] <- 0.5
-  alpha[3] <- 0.5
   
-  # Transport | Race, Site
-  for (i in 1:N){
-    Transport[i] ~ dbern(pi_T[i])
-    logit(pi_T[i]) <- gamma0
-                      + gamma_site[Site[i]]
-                      + gamma_race[Race[i]]
-  }
-  gamma0 ~ dnorm(0, 0.001)
-  for (s in 1:9){ gamma_site[s] ~ dnorm(0, 0.001) }
-  for (r in 1:3){ gamma_race[r] ~ dnorm(0, 0.001) }
+  eta0 ~ dnorm(0, 0.001)
+  for (s in 1:9){ eta_site[s] ~ dnorm(0, 0.001) }
+  for (r in 1:3){ eta_race[r] ~ dnorm(0, 0.001) }
+  for (q in 1:8){ eta_time[q] ~ dnorm(0, 0.001) }
   
-  # Notify | Transport, Race, Site
+  tau_A ~ dgamma(0.001, 0.001)
+  sigma_A <- 1 / sqrt(tau_A)
+  
+  # Notify | Transport, Race, Site, Z
   # Transport[i] = 1 if EMS, 0 if car
   for (i in 1:N){
     Notify[i] ~ dbern(pi_N[i])
@@ -52,28 +46,41 @@ model {
     logit(pi_EMS[i]) <- delta0
                         + delta_site[Site[i]]
                         + delta_race[Race[i]]
+                        + delta_time[Time[i]]
   }
   delta0 ~ dnorm(0, 0.001)
   for (s in 1:9){ delta_site[s] ~ dnorm(0, 0.001) }
   for (r in 1:3){ delta_race[r] ~ dnorm(0, 0.001) }
-  
-  # Age | Notify, Transport, Race, Site
+  for (q in 1:8){ delta_time[q] ~ dnorm(0, 0.001) }
+
+  # Transport | Race, Site, Z
   for (i in 1:N){
-    Age[i] ~ dnorm(mu_A[i], tau_A)
-    mu_A[i] <- eta0
-              + eta_site[Site[i]]
-              + eta_race[Race[i]]
-              + eta_transport * Transport[i]
-              + eta_notify * Notify[i]
+    Transport[i] ~ dbern(pi_T[i])
+    logit(pi_T[i]) <- gamma0
+                      + gamma_site[Site[i]]
+                      + gamma_Thromb * hadThrombectomy[i]
+                      + gamma_TPA * hadTPA[i]
+                      + gamma_thrComp * thrComplic[i]
+                      + gamma_tpaComp * tpaComplic[i]
   }
-  eta0 ~ dnorm(0, 0.001)
-  for (s in 1:9){ eta_site[s] ~ dnorm(0, 0.001) }
-  for (r in 1:3){ eta_race[r] ~ dnorm(0, 0.001) }
-  eta_transport ~ dnorm(0, 0.001)
-  eta_notify ~ dnorm(0, 0.001)
+  gamma0 ~ dnorm(0, 0.001)
+  for (s in 1:9){ gamma_site[s] ~ dnorm(0, 0.001) }
+  gamma_Thromb ~ dnorm(0, 0.001)
+  gamma_TPA ~ dnorm(0, 0.001)
+  gamma_thrComp ~ dnorm(0, 0.001)
+  gamma_tpaComp ~ dnorm(0, 0.001)
   
-  tau_A ~ dgamma(0.001, 0.001)
-  sigma_A <- 1/sqrt(tau_A)
+  # Race | Site, Z
+  for (i in 1:N){
+    Race[i] ~ dcat(pi_race[Site[i],1:3])
+  }
+  for (s in 1:9){
+    pi_race[s,1:3] ~ ddirch(alpha[1:3])
+  }
+  
+  alpha[1] <- 0.5  # Jeffreys prior
+  alpha[2] <- 0.5
+  alpha[3] <- 0.5
   
   
   # Priors for outcome model
